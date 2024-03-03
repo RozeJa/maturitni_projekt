ec81a2db20f928b287f2d193a267a08f18dbc590
RozeJa <92266408+RozeJa@users.noreply.github.com>
2024-03-03 15:19:18 +0100

    update zavislosti

--- cinema_town/src/main/java/cz/rozek/jan/cinema_town/repositories/FilmRepository.java
+++ cinema_town/src/main/java/cz/rozek/jan/cinema_town/repositories/FilmRepository.java
@@ -10,8 +10,6 @@ import cz.rozek.jan.cinema_town.models.primary.People;
 
 public interface FilmRepository extends MongoRepository<Film, String> {
 
-  // TODO ! FUNGUJE??
-  List<Film> findByDirector(People people);
   List<Film> findByActors(People people);
 
   List<Film> findByGenres(Genre genre); 
